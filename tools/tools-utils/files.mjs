339eb49b393b9205f841ae52b52f87df0b59772f
Tasso Evangelista <tasso.evangelista@rocket.chat>
2024-01-08 21:45:45 -0300

    Upgrade `rimraf` and `fast-glob`

--- tools/tools-utils/files.mjs
+++ tools/tools-utils/files.mjs
@@ -1,7 +1,7 @@
 import { lstat, readlink, readFile, writeFile, mkdir } from 'node:fs/promises';
 import { dirname, join } from 'node:path';
 import { promisify } from 'node:util';
-import rimraf from 'rimraf';
+import { rimraf } from 'rimraf';
 import fg from 'fast-glob';
 import { encodeJson, decodeJson } from './json.mjs';
 import { encodeJson5, decodeJson5 } from './json5.mjs';
@@ -38,7 +38,7 @@ export const writeJson5 = (path) => (data) => {
   return data;
 };
 
-export const removeFile = (path) => promisify(rimraf)(path);
+export const removeFile = (path) => rimraf.rimraf(path);
 
 export const listFiles = (patterns, cwd) =>
   fg(patterns, { cwd }).then((paths) =>
